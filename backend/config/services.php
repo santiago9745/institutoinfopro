8db684a321d8c9ae5567e000537bed0b5981e803
santiago9745 <cossio.santiago.132@gmail.com>
2025-07-13 01:16:05 -0400

    posible arreglo del error de carga y agregado de la interfaz de notas para el estudiante

--- backend/config/services.php
+++ backend/config/services.php
@@ -30,5 +30,9 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
+    'passport' => [
+        'client_id' => env('CLIENT_ID'),
+        'client_secret' => env('CLIENT_SECRET'),
+        'grant_type' => 'password',
+    ],
 ];
---
+++ frontend/src/layouts/grades/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import MDBox from 'components/MDBox';
+import MDTypography from 'components/MDTypography';
+import MDAlert from 'components/MDAlert';
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import DataTable from "examples/Tables/DataTable";
+import Footer from 'examples/Footer';
+
+const Notas = () => {
+  return (
+    <DashboardLayout>
+        <DashboardNavbar/>
+            <MDBox pt={6} pb={3}>
+                <Grid container spacing={6}>
+                    <Grid item xs={12}>
+                        <Card>
+                            <MDBox
+                                mx={2}
+                                mt={-3}
+                                py={3}
+                                px={2}
+                                variant="gradient"
+                                bgColor="info"
+                                borderRadius="lg"
+                                coloredShadow="info"
+                            >
+                                <MDTypography variant="h6" color="white">
+                                    Listado de notas 
+                                </MDTypography>
+                            </MDBox>
+                            <MDBox pt={3}>
+                                
+                            </MDBox>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </MDBox>
+        <Footer />
+    </DashboardLayout>
+  );
+};
+
+export default Notas;
